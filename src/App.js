ba994ff59166f93343e7d8e9510586ee5026f185
abraham171996 <ibrahimmemmed3131908@gmail.com>
2023-07-21 01:22:23 +0400

    feat:add movie details in ui

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-
+import StarRating from "./StarRating";
 const tempMovieData = [
   {
     imdbID: "tt1375666",
@@ -215,9 +215,50 @@ function Movie({ movie ,onSelectMovie}) {
 }
 
 function MovieDetails({ selectedId ,onCloseMovie}) {
+  const [movie,setMovie] = useState({})
+  const [isLoading,setIsLoading] = useState(false)
+  const {Title:title,Year:year,Poster:poster,Runtime:runtime,imdbRating,Plot:plot,Released:released,Actors:actors,Director:director,Genre:genre} = movie;
+  useEffect(() => {
+    async function getMovieDetails() {
+        setIsLoading(true)
+        const res = await fetch(
+          `http://www.omdbapi.com/?apikey=${KEY}&i=${selectedId}`
+        );
+        
+        const data = await res.json();
+        setMovie(data);
+        setIsLoading(false)  
+    }
+    getMovieDetails();
+  }, [selectedId]);
+  
   return <div className="details">
+    { isLoading ? <Loader/> :
+    <>
+    <header>
     <button className="btn-back" onClick={onCloseMovie}>&larr;</button>
-    {selectedId}</div>;
+    <img src={poster} alt={`Poster of ${movie} movie`}/>
+    <div className="details-overview">
+      <h2>{title}</h2>
+      <p>{released} &bull; {runtime}</p>
+      <p>{genre}</p>
+      <p><span>⭐</span>
+        {imdbRating}
+      </p>
+
+    </div>
+    </header>
+    <section>
+      <div className="rating">
+      <StarRating maxRating={10} size={24}/>
+      </div>
+      <p><em>{plot}</em></p>
+      <p>Starring {actors}</p>
+      <p>Directed by {director}</p>
+    </section>
+    </>
+}
+    </div>;
 }
 function WatchedSummary({ watched }) {
   const avgImdbRating = average(watched.map((movie) => movie.imdbRating));
